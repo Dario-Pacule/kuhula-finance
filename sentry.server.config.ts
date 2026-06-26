aa71539b5daf7ba1f5b737a05fbac6805aeeb9e7
unknown <localadmin@TTCOMP28.tablutech.local>
2026-06-26 09:13:24 +0200

    chore: configurar Sentry Next.js SDK para monitorização de erros

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://1dd7646229caad5fa6bc3985c35e7f38@o4511630388035584.ingest.de.sentry.io/4511630415954000",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
